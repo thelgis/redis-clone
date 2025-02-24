1939b487d089083728e449453272cd12c3308cc6
Thelgis Kotsos <thelgis@pm.me>
2025-02-24 19:24:05 +0200

    Bind to a port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("Accepted new connection");
+            }
+            Err(error) => {
+                println!("Error when accepting connection: {}", error);
+            }
+        }
+    }
+
 }
